c8ff5736bf72615ff5ad8f757adcac78f0f4f77f
anneleyder <anneleyder@gmail.com>
2017-05-12 12:06:18 +0200

    migration lienTables

---
+++ migrations/m170512_094156_lienTables.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170512_094156_lienTables extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170512_094156_lienTables cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
