bc2d5654ae7d093f83d2617356dee67af49bc831
anneleyder <anneleyder@gmail.com>
2017-05-12 13:09:06 +0200

    2 eme essais pour lienTables

--- migrations/m170512_094156_lienTables.php
+++ migrations/m170512_094156_lienTables.php
@@ -6,6 +6,33 @@ class m170512_094156_lienTables extends Migration
 {
     public function up()
     {
+       // add foreign key 1 for table actionAgent
+        $this->addForeignKey(
+            'fk-actionAgent_actionPO',
+            'actionAgent',
+            'actionAgent_idActionPO',
+            'actionPO',
+            'actionPO_id',
+            'SET NULL'
+        );
+       // add foreign key 2 for table actionAgent
+        $this->addForeignKey(
+            'fk-actionAgent-agent',
+            'actionAgent',
+            'actionAgent_idAgent',
+            'actionAgent',
+            'actionAgent_id',
+            'SET NULL'
+        );
+       // add foreign key for table actionPO
+        $this->addForeignKey(
+            'fk-actionPO-etage',
+            'actionPO',
+            'etage_id',
+            'etage',
+            'etage_id',
+            'SET NULL'
+        );
 
     }
 
