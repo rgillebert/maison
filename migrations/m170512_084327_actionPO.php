5005a2bab20f07a16d947296724f8d49e8f6b3f4
WatchmeJMD <jean-marie.dubois@watchme.be>
2017-05-12 11:27:01 +0200

    add table actionPO

---
+++ migrations/m170512_084327_actionPO.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+class m170512_084327_actionPO extends Migration
+{
+    public function up()
+    {
+        echo 'create the actionPO';
+        $this->createTable('actionPO', [
+            'actionPO_id' => $this->primaryKey(),
+            'actionPO_annee' => $this->int(),
+            'actionPO_action' => $this->string(),
+            'actionPO_description' => $this->string(),
+            'etage_id' => $this->int(),
+            'actionAgent_id' => $this->int()
+        ]);
+    }
+
+    public function down()
+    {
+        echo "drop the ActionPO table";
+        $this->dropTable('actionPO');
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
